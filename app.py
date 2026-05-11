5f59d667d8e13fa25e28768d49c2f8c41404cc00
Rayan <rayanirfank@gmail.com>
2026-05-12 00:14:18 +0300

    Optimize inference memory

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def upload_image():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
