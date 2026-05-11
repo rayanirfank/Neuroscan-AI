a3cb683109fbc5b1d5396511e8799b96d89af748
Rayan <rayanirfank@gmail.com>
2026-05-11 23:45:37 +0300

    Optimize inference memory

--- app.py
+++ app.py
@@ -18,20 +18,14 @@ app = Flask(__name__)
 
 CORS(app)
 
-# Upload folder
-
 UPLOAD_FOLDER = "uploads"
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Load trained model
-
-mmodel = load_model(
+model = load_model(
     "brain_tumor_model.keras",
     compile=False
 )
-
-# Class labels
 
 classes = [
     "Glioma",
@@ -40,15 +34,12 @@ classes = [
     "Pituitary"
 ]
 
-# Home route
-
 @app.route('/')
 
 def home():
 
     return render_template("Dashboard.html")
 
-# About route
 
 @app.route('/about')
 
@@ -56,7 +47,6 @@ def about():
 
     return render_template("About.html")
 
-# Upload + Prediction route
 
 @app.route('/upload', methods=['POST'])
 
@@ -75,10 +65,8 @@ def upload_image():
         if file.filename == '':
 
             return jsonify({
-                "error": "Empty filename"
+                "error": "No file selected"
             }), 400
-
-        # Save uploaded file
 
         file_path = os.path.join(
             UPLOAD_FOLDER,
@@ -87,38 +75,28 @@ def upload_image():
 
         file.save(file_path)
 
-        # Load image
-
         img = image.load_img(
             file_path,
             target_size=(240, 240)
         )
 
-        # Convert image to array
-
         img_array = image.img_to_array(img)
-
-        # Expand dimensions
 
         img_array = np.expand_dims(
             img_array,
             axis=0
         )
 
-        # Preprocess image
-
         img_array = preprocess_input(img_array)
 
-        # Model prediction
-
         prediction = model.predict(
-    img_array,
-    verbose=0
-)
+            img_array,
+            verbose=0
+        )
 
-predicted_class = classes[
-    np.argmax(prediction)
-]
+        predicted_class = classes[
+            np.argmax(prediction)
+        ]
 
         confidence = float(
             np.max(prediction) * 100
@@ -134,19 +112,18 @@ predicted_class = classes[
 
     except Exception as e:
 
-        print("ERROR:", str(e))
-
         return jsonify({
 
             "error": str(e)
 
         }), 500
 
-# Run app
 
 if __name__ == '__main__':
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(
+        os.environ.get("PORT", 5000)
+    )
 
     app.run(
         host='0.0.0.0',
